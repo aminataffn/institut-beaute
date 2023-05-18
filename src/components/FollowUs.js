104a04f1759584bbf93bf13efe43ebe58231cb16
ARGENTEUIL <ARGENTEUIL@MacBook-Pro-de-Djibril.local>
2023-05-18 09:27:07 +0200

    Création de la constante photo

--- src/components/FollowUs.js
+++ src/components/FollowUs.js
@@ -5,16 +5,25 @@ import insta2 from '../assets/insta-2.jpeg'
 import insta3 from '../assets/insta-3.jpeg'
 import insta4 from '../assets/insta-4.jpeg'
 
+const photos = [
+  { src: insta1 },
+  { src: insta2 },
+  { src: insta3 },
+  { src: insta4 }
+];
+
+
 function FollowUs() {
     return(
       <div className='follow-us'>
         <p className='follow-us-title'>SUIVEZ-NOUS SUR </p>
         <img src={instagram} className='instagram' />
         <ul className='follow-us-gallery'>
-          <li className='follow-us-photo'><img src={insta1}  /></li>
-          <li className='follow-us-photo'><img src={insta2} /></li>
-          <li className='follow-us-photo'><img src={insta3} /></li>
-          <li className='follow-us-photo'><img src={insta4} /></li>
+          {photos.map((photo, index) => (
+          <li className='follow-us-photo' key={index}>
+            <img src={photo.src} alt={`Instagram photo ${index + 1}`} />
+          </li>
+          ))}
         </ul>
         <p className='follow-us-alt'>@BEAUTYNCARE92</p>
       </div>
