b835f38b1f17cd228fcbc76534f60f1cb6425c73
ARGENTEUIL <ARGENTEUIL@openroam-prg-hf-1-135-142.net.univ-paris-diderot.fr>
2023-06-07 14:41:24 +0200

    ajout class Category

--- src/datas/prestations.js
+++ src/datas/prestations.js
@@ -1,8 +1,7 @@
-class Prestations {
-    constructor(name, category, subcategory, isFixedPrice, price) {
+class Prestation {
+    constructor(name, category, isFixedPrice, price) {
         this.name = name;
         this.category = category;
-        this.subcategory = subcategory;
         this.isFixedPrice = isFixedPrice;
         this.price = price;
     }
@@ -15,30 +14,40 @@ class Prestations {
     }
 }
 
+class Category {
+    constructor(name) {
+        this.name = name;
+        this.prestations = [];
+    }
+     addNewPrestation(name, isFixedPrice, price){
+        const prestation = new Prestation(name, this.name, isFixedPrice, price)
+     }
+}
+
 export const prestations = [ 
-    new Prestations('Shampoing + Brushing - Cheveux courts','Cheveux', 'Brushing', true, 20),
-    new Prestations('Shampoing + Brushing - Cheveux mi-longs','Cheveux', 'Brushing', true, 25),
-    new Prestations('Shampoing + Brushing - Cheveux longs','Cheveux', 'Brushing', true, 30),
+    new Prestation('Shampoing + Brushing - Cheveux courts','Cheveux', true, 20),
+    new Prestation('Shampoing + Brushing - Cheveux mi-longs','Cheveux', true, 25),
+    new Prestation('Shampoing + Brushing - Cheveux longs','Cheveux', true, 30),
     
-    new Prestations('Lissage Nano Indien + Shampooing + Brushing - Cheveux courts','Cheveux', 'Lissage Nano Indien', false, 180),
-    new Prestations('Lissage Nano Indien + Shampooing + Brushing - Cheveux mi-longs','Cheveux', 'Lissage Nano Indien', false, 190),
-    new Prestations('Lissage Nano Indien + Shampooing + Brushing - Cheveux longs','Cheveux', 'Lissage Nano Indien', false, 200),
+    new Prestation('Lissage Nano Indien + Shampooing + Brushing - Cheveux courts','Cheveux', false, 180),
+    new Prestation('Lissage Nano Indien + Shampooing + Brushing - Cheveux mi-longs','Cheveux', false, 190),
+    new Prestation('Lissage Nano Indien + Shampooing + Brushing - Cheveux longs','Cheveux', false, 200),
 
-    new Prestations('Lissage Tanin + Shampooing + Brushing - Cheveux courts','Cheveux', 'Lissage Tanin', false, 120),
-    new Prestations('Lissage Tanin + Shampooing + Brushing - Cheveux mi-longs','Cheveux', 'Lissage Tanin', false, 130),
-    new Prestations('Lissage Tanin + Shampooing + Brushing - Cheveux longs','Cheveux', 'Lissage Tanin', false, 140),
+    new Prestation('Lissage Tanin + Shampooing + Brushing - Cheveux courts','Cheveux', false, 120),
+    new Prestation('Lissage Tanin + Shampooing + Brushing - Cheveux mi-longs','Cheveux', false, 130),
+    new Prestation('Lissage Tanin + Shampooing + Brushing - Cheveux longs','Cheveux', false, 140),
 
-    new Prestations('Lissage Organic Gold + Shampooing + Brushing - Cheveux courts','Cheveux', 'Lissage Organic Gold', false, 140),
-    new Prestations('Lissage Organic Gold + Shampooing + Brushing - Cheveux mi-longs','Cheveux', 'Lissage Organic Gold', false, 150),
-    new Prestations('Lissage Organic Gold + Shampooing + Brushing - Cheveux longs','Cheveux', 'Lissage Organic Gold', false, 160),
+    new Prestation('Lissage Organic Gold + Shampooing + Brushing - Cheveux courts','Cheveux', false, 140),
+    new Prestation('Lissage Organic Gold + Shampooing + Brushing - Cheveux mi-longs','Cheveux', false, 150),
+    new Prestation('Lissage Organic Gold + Shampooing + Brushing - Cheveux longs','Cheveux', false, 160),
 
-    new Prestations('Botox Capillaire + Shampooing + Brushing - Cheveux court','Cheveux', 'Botox Capillaire', false, 60),
-    new Prestations('Botox Capillaire + Shampooing + Brushing - Cheveux mi-longs','Cheveux', 'Botox Capillaire', false, 70),
-    new Prestations('Botox Capillaire + Shampooing + Brushing - Cheveux longs','Cheveux', 'Botox Capillaire', false, 80),
+    new Prestation('Botox Capillaire + Shampooing + Brushing - Cheveux court','Cheveux', false, 60),
+    new Prestation('Botox Capillaire + Shampooing + Brushing - Cheveux mi-longs','Cheveux', false, 70),
+    new Prestation('Botox Capillaire + Shampooing + Brushing - Cheveux longs','Cheveux', false, 80),
 
-    new Prestations('Cryothérapie Capillaire + Shampooing + Brushing - Cheveux courts','Cheveux', 'Cryothérapie Capillaire', false, 70),
-    new Prestations('Cryothérapie Capillaire + Shampooing + Brushing - Cheveux mi-longs','Cheveux', 'Cryothérapie Capillaire', false, 80),
-    new Prestations('Cryothérapie Capillaire + Shampooing + Brushing - Cheveux longs','Cheveux', 'Cryothérapie Capillaire', false, 90),
+    new Prestation('Cryothérapie Capillaire + Shampooing + Brushing - Cheveux courts','Cheveux', false, 70),
+    new Prestation('Cryothérapie Capillaire + Shampooing + Brushing - Cheveux mi-longs','Cheveux', false, 80),
+    new Prestation('Cryothérapie Capillaire + Shampooing + Brushing - Cheveux longs','Cheveux', false, 90),
 ]
 
 export const categories = ['Cheveux']
